52f656301f0e3a9841a6dcbdc768f36d6e5bc261
Jose Medellin <kabo.0699@gmai.com>
2025-05-10 17:58:03 -0600

    config scripts and husky

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap()
+  .then(() => {
+    console.log('Server running on port', process.env.PORT ?? 3000);
+  })
+  .catch((err) => console.error('Error starting server', err));
